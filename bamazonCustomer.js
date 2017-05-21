e8d447dcc180f1a1f89d8165509cbc34d2ddc462
John <jlev2282@gmail.com>
2017-05-21 15:20:02 -0400

    database connected and inventory plus first two prompts to client

---
+++ bamazonCustomer.js
@@ -0,0 +1,45 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+	host:"localhost",
+	port: 3306,
+	user: "root",
+	password:"localhost",
+	database: "Bamazon_db"
+})
+
+connection.connect(function(err){
+	console.log("Connected as Id:"+connection.threadId);
+	Inventory();
+})
+
+var Inventory = function(){
+	connection.query("SELECT * FROM products", function(err,res){
+		console.log(res);
+	inquirer.prompt([{
+		name:"id",
+		type: "input",
+		message: "What is the ID of the item you'd like to buy?",
+		validate: function(value){
+			if(isNaN(value)==false){
+				return true;
+			} else {
+				return false;
+			}
+		}
+		},{
+		name:"startingBid",
+		type: "input",
+		// message: "How many units of "+answer.id+" would you like to buy?"
+		message: "How many units would you like to buy?",
+		validate: function(value){
+			if(isNaN(value)==false){
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}])
+})
+}
